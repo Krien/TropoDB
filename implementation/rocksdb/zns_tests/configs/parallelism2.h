dd37a7e17e8a1c5ef7fee83c631eeeec5284c381
Krijn Doekemeijer <krijn.doekemeijer@gmail.com>
2022-08-13 18:20:58 +0200

    Update parallelism2.h

--- implementation/rocksdb/zns_tests/configs/parallelism2.h
+++ implementation/rocksdb/zns_tests/configs/parallelism2.h
@@ -50,10 +50,11 @@ constexpr static size_t wal_concurrency =
 // L0 and LN options
 constexpr static size_t L0_zones =
     100; /**< amount of zones to reserve for each L0 circular log */
-constexpr static uint8_t L0_circular_logs =
+constexpr static uint8_t lower_concurrency  =
     2; /**< Number of L0 circular logs. Increases parallelism. */
+constexpr static size_t wal_manager_zone_count = wal_count / lower_concurrency;
 constexpr static int L0_slow_down =
-    60; /**< Amount of SSTables in L0 before client puts will be stalled. Can
+    80; /**< Amount of SSTables in L0 before client puts will be stalled. Can
            stabilise latency. Setting this too high can cause some clients to
            wait for minutes during heavy background I/O.*/
 static constexpr uint8_t number_of_concurrent_L0_readers =
@@ -63,12 +64,12 @@ static constexpr uint8_t number_of_concurrent_LN_readers =
 constexpr static size_t min_ss_zone_count =
     5; /**< Minimum amount of zones for L0 and LN each*/
 constexpr static double ss_compact_treshold[level_count]{
-    4.,
-    8. * 1024. * 1024.,
-    32. * 1024. * 1024.,
-    128. * 1024. * 1024.,
-    512. * 1024. * 1024.,
-    2048. * 1024. * 1024.}; /**< Size of each level before we want
+    8.,
+    16. * 1024. * 1024. * 1024.,
+    16. * 4. * 1024. * 1024. * 1024.,
+    16. * 16. * 1024. * 1024. * 1024.,
+    16. * 64. * 1024. * 1024. * 1024.,
+    16. * 256. * 1024. * 1024. * 1024.}; /**< Size of each level before we want
 compaction. L0 is in number of SSTables, L1 and up in bytes. */
 constexpr static double ss_compact_treshold_force[level_count]{
     0.85, 0.95, 0.95, 0.95,
@@ -77,21 +78,21 @@ constexpr static double ss_compact_treshold_force[level_count]{
                     full and compaction is FORCED. This treshold prevents out of
                     space issues. E.g. L0 may not be filled more than 85%.*/
 constexpr static double ss_compact_modifier[level_count]{
-    0, 0, 16, 8,
+    64, 32, 16, 8,
     4, 1}; /**< Modifier for each compaction treshold. When size reaches above
              the compaction treshold, certain compactions might be more
              important than others. For example, setting a high modifier for 2
              causes compactions in 2 to be done first. 0 and 1 are ignored as
              they are handled in a separate thread */
 constexpr static uint64_t max_bytes_sstable_l0 =
-    1024U * 1024U * 64; /**< Maximum size of SSTables in L0. Determines the
+    1024U * 1024U * 512; /**< Maximum size of SSTables in L0. Determines the
                 amount of tables generated on a flush. Rounded to round number
                 of lbas by TropoDB. */
 constexpr static uint64_t max_bytes_sstable_ = (uint64_t)(
     1073741824. * 2. * 0.95); /**< Maximum size of SSTables in LN. LN tables
                                  reserve entire zones, therefore, please set
                                  to a multitude of approximately n zones */
-constexpr static uint64_t max_lbas_compaction_l0 = 2097152 * 6; /**< Maximum
+constexpr static uint64_t max_lbas_compaction_l0 = 2097152 * 12; /**< Maximum
 amount of LBAS that can be considered for L0 to LN compaction. Prevents OOM.*/
 
 // Compaction
@@ -106,7 +107,7 @@ constexpr static bool compaction_allow_deferring_writes =
              thread. This can allow compaction to continue without waiting on
              writes to finish.*/
 constexpr static uint8_t compaction_maximum_deferred_writes =
-    4; /**< How many SSTables can be deferred at most. Be careful, setting
+    6; /**< How many SSTables can be deferred at most. Be careful, setting
 this too high can cause OOM.*/
 
 // Containerisation
